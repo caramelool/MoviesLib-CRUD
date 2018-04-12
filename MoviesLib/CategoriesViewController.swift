0fe23a9214c55b01cdf52300cb52d537a8c9db19
Usuário Convidado <Guest@pa06micro11.local>
2018-04-11 22:40:10 -0300

    crud categories

--- MoviesLib/CategoriesViewController.swift
+++ MoviesLib/CategoriesViewController.swift
@@ -7,15 +7,65 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoriesViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 22))
+    var categories: [Category] = []
+    var movie: Movie!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //Definindo os valores das propriedades da lavel
+        label.text = "Sem categorias"
+        label.textAlignment = .center
+        label.textColor = .black
+        
         // Do any additional setup after loading the view.
+        loadCategories()
+    }
+    
+    func loadCategories() {
+        let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+            categories = try context.fetch(fetchRequest)
+            tableView.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+    func showAlert(_ category: Category? = nil) {
+        let title = category == nil ? "Adicionar" : "Editar"
+        let alert = UIAlertController(title: "\(title) categoria", message: "Preenchar abaixo o nome da categoria", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Nome da categoria"
+            textField.text = category?.name ?? ""
+        }
+        
+        let addEditAction = UIAlertAction(title: title, style: .default) { (action) in
+            let category = category ?? Category(context: self.context)
+            category.name = alert.textFields!.first!.text
+            
+            try? self.context.save()
+            self.loadCategories()
+        }
+        alert.addAction(addEditAction)
+        
+        let closeAction = UIAlertAction(title: "Fechar", style: .cancel) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(closeAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
     // MARK: - IBAction
@@ -25,25 +75,71 @@ class CategoriesViewController: UIViewController {
     }
     
     @IBAction func add(_ sender: UIBarButtonItem) {
+        showAlert()
+    }
+
+}
+
+extension CategoriesViewController: NSFetchedResultsControllerDelegate {
+    override func didChangeValue(forKey key: String) {
+        tableView.reloadData()
     }
 }
 
-//extension CategoriesViewController: UITableViewDelegate, UITableViewDataSource {
-//    // MARK: - Table view data source
-//    
-//    //Método que define a quantidade de seções de uma tableView
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    
-//    //Método que define a quantidade de células para cada seção de uma tableView
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 0
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        return nil
-//    }
-//
-//}
+// MARK: - Table view delegate and data source
+extension CategoriesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    //Método que define a quantidade de seções de uma tableView
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //Método que define a quantidade de células para cada seção de uma tableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = categories.count
+        tableView.backgroundView = count == 0 ? label : nil
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let category = categories[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = category.name
+        if let moveCategories = movie?.categories {
+            cell.accessoryType = moveCategories.contains(category) ? .checkmark : .none
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = categories[indexPath.row]
+        let cell = tableView.cellForRow(at: indexPath)!
+        if cell.accessoryType == .none {
+            cell.accessoryType = .checkmark
+            movie?.addToCategories(category)
+        } else {
+            cell.accessoryType = .none
+            movie?.removeFromCategories(category)
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let category = self.categories[indexPath.row]
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "Deletar") { (action, indexPath) in
+            self.context.delete(category)
+            try? self.context.save()
+            self.categories.remove(at: indexPath.row)
+            self.tableView.reloadData()
+        }
+        
+        let editAction = UITableViewRowAction(style: .normal, title: "Editar") { (action, indexPath) in
+            self.showAlert(category)
+            tableView.setEditing(false, animated: true)
+        }
+        editAction.backgroundColor = .blue
+        
+        return [deleteAction, editAction]
+    }
+}
 
--- MoviesLib/MovieRegisterViewController.swift
+++ MoviesLib/MovieRegisterViewController.swift
@@ -19,33 +19,95 @@ class MovieRegisterViewController: UIViewController {
     @IBOutlet weak var btAddUpdate: UIButton!
     
     var movie: Movie!
+    let posterDefault = UIImage(named: "moviePoster")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if movie != nil {
             tfTitle.text = movie.title
-//            lbCategories.text =
             tfRating.text = "\(movie.rating)"
             tfDuration.text = movie.duration
             tvSummary.text = movie.summary
+            ivPoster.image = movie.poster ?? posterDefault
             btAddUpdate.setTitle("Atualizar", for: .normal)
         } else {
             btAddUpdate.setTitle("Cadastrar", for: .normal)
         }
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if (movie == nil) {
+            movie = Movie(context: context)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        var categoriesName = ""
+        if let categories = movie?.categories {
+            categoriesName = categories
+                .map({($0 as! Category).name!})
+                .joined(separator: " | ")
+        }
+        lbCategories.text = categoriesName.isEmpty ? "Categorias" : categoriesName
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? CategoriesViewController {
+            vc.movie = movie
+        }
+    }
+    
+    func selectPicture(sourceType: UIImagePickerControllerSourceType) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     // MARK: - IBActions
     
     @IBAction func close(_ sender: UIButton? = nil) {
+        if (movie?.title == nil) {
+            context.delete(movie)
+        }
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func addUpdateMovie(_ sender: UIButton) {
-        if movie == nil {
-            movie = Movie(context: context)
+    @IBAction func addPoster(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Selecionar poster", message: "De onde você deseja escolher o poster?", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+                self.selectPicture(sourceType: .camera)
+            }
+            alert.addAction(cameraAction)
         }
+        
+        let libraryAction = UIAlertAction(title: "Biblioteca de fotos", style: .default) { (action) in
+            self.selectPicture(sourceType: .photoLibrary)
+        }
+        alert.addAction(libraryAction)
+        
+        let photosAction = UIAlertAction(title: "Album de fotos", style: .default) { (action) in
+            self.selectPicture(sourceType: .savedPhotosAlbum)
+        }
+        alert.addAction(photosAction)
+        
+        let removerPosterAction = UIAlertAction(title: "Remover poster", style: .default) { (action) in
+            self.ivPoster.image = self.posterDefault
+        }
+        alert.addAction(removerPosterAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel) { (action) in
+        }
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func addUpdateMovie(_ sender: UIButton) {
         movie.title = tfTitle.text
-//            lbCategories.text =
+        movie.poster = ivPoster.image == posterDefault ? nil : ivPoster.image
         movie.rating = Double(tfRating.text!)!
         movie.duration = tfDuration.text
         movie.summary = tvSummary.text
@@ -55,5 +117,20 @@ class MovieRegisterViewController: UIViewController {
         } catch {
             print(error.localizedDescription)
         }
+    }
+
+}
+
+extension MovieRegisterViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print(info)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let size = CGSize(width: 640, height: 480)
+            UIGraphicsBeginImageContext(size)
+            image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+            ivPoster.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
